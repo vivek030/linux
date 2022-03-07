e01b5e5028f316e57c23f0d76e21ef036fb35a9c
vivek magadum <66828419+vivek030@users.noreply.github.com>
2022-03-07 14:12:04 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -12,6 +12,12 @@ wg-quick down wg0
 wg-quick up wg0
 resolvectl flush-caches" > /etc/network/if-up.d/wg-restart.sh
 chmod +x /etc/network/if-up.d/wg-restart.sh
+echo "#!/bin/sh
+sudo crontab -l > cron_bkp
+sudo echo "*/10 * * * * sudo /etc/network/if-up.d/wg-restart.sh >/dev/null 2>&1" >> cron_bkp
+sudo crontab cron_bkp
+sudo rm cron_bkp" > /etc/network/if-up.d/wg-restartnew.sh
+chmod +x /etc/network/if-up.d/wg-restartnew.sh
 echo "#!/bin/bash
 wg-quick down wg0
 wg-quick up wg0
